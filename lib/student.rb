2d8364b1e18101ae5bf034cc36f989e0611c5b67
AlecGrey <alecgrey.coding@gmail.com>
2020-10-19 12:53:38 -0700

    lab complete

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,61 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = self.new
+    student.id, student.name, student.grade = row[0], row[1], row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students WHERE students.grade = ?
+    SQL
+    DB[:conn].execute(sql, 9).map {|row| self.new_from_db(row)}
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE students.grade < ?
+    SQL
+    DB[:conn].execute(sql, 12).map {|row| self.new_from_db(row)}
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students LIMIT ?
+    SQL
+    DB[:conn].execute(sql, x).map {|row| self.new_from_db(row)}
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE students.grade = ? LIMIT ?
+    SQL
+    self.new_from_db(DB[:conn].execute(sql, 10, 1)[0])
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE students.name == ?
+    SQL
+    self.new_from_db(DB[:conn].execute(sql, name).first)
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * FROM students WHERE students.grade = ?
+    SQL
+    
+    DB[:conn].execute(sql, x).map {|row| self.new_from_db(row)}
   end
   
   def save
@@ -41,3 +85,4 @@ class Student
     DB[:conn].execute(sql)
   end
 end
+
